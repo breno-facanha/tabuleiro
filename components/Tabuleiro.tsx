6c12207d5b1b06e95cc14f75b813647abb95920b
Breno Façanha <brenofacanha.ti@gmail.com>
2025-01-13 20:10:45 -0300

    criado componente tabuleiro

---
+++ components/Tabuleiro.tsx
@@ -0,0 +1,14 @@
+export default function Tabuleiro(){
+    return (
+        <div>
+            <div className="h-24 w-24 bg-white"></div>
+            <div className="h-24 w-24 bg-black"></div>
+            <div className="h-24 w-24 bg-white"></div>
+            <div className="h-24 w-24 bg-black"></div>
+            <div className="h-24 w-24 bg-white"></div>
+            <div className="h-24 w-24 bg-black"></div>
+            <div className="h-24 w-24 bg-white"></div>
+            <div className="h-24 w-24 bg-black"></div>
+        </div>
+    )
+}
