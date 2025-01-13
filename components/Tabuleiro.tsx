6d9df718f0c1ac231989a3ed7962a42529788854
Breno Façanha <brenofacanha.ti@gmail.com>
2025-01-13 20:34:43 -0300

    adicionado props no componente Tabuleiro e passando parametro no componente para mudar uma propieadade do tailwindcss

--- components/Tabuleiro.tsx
+++ components/Tabuleiro.tsx
@@ -1,6 +1,12 @@
-export default function Tabuleiro(){
+interface propsTabuleiro {
+    inverso: string
+}
+
+export default function Tabuleiro(props: propsTabuleiro){
     return (
-        <div>
+        <div className={`
+        flex flex-${props.inverso} justify-center items-center
+        `}>
             <div className="h-24 w-24 bg-white"></div>
             <div className="h-24 w-24 bg-black"></div>
             <div className="h-24 w-24 bg-white"></div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
+import Tabuleiro from "@/components/Tabuleiro";
 
 export default function Home() {
   return (
-    <div>ola</div>
+    <div className="h-screen w-full flex justify-center items-center bg-slate-500">
+      <div >
+        <Tabuleiro inverso="row" />
+        <Tabuleiro inverso="row-reverse" />
+        <Tabuleiro inverso="row" />
+        <Tabuleiro inverso="row-reverse" />
+        <Tabuleiro inverso="row" />
+        <Tabuleiro inverso="row-reverse" />
+        <Tabuleiro inverso="row" />
+        <Tabuleiro inverso="row-reverse" />
+      </div>
+    </div>
   );
 }
