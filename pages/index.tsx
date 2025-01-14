e4e0e4da698b45eacf0d8a48bd316e8fc4c88233
Breno Façanha <brenofacanha.ti@gmail.com>
2025-01-13 23:48:52 -0300

    ajustando display flex

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Tabuleiro from "@/components/Tabuleiro";
 export default function Home() {
   return (
     <div className="h-screen w-full flex justify-center items-center bg-slate-500">
-      <div >
+      <div className="flex flex-col">
         <Tabuleiro inverso="row" />
         <Tabuleiro inverso="row-reverse" />
         <Tabuleiro inverso="row" />
