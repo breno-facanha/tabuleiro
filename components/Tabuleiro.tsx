311380950bdb1103832b9faa6269c6a88a3c454e
Breno Façanha <brenofacanha.ti@gmail.com>
2025-01-14 11:54:36 -0300

    ajustes tailwind

--- components/Tabuleiro.tsx
+++ components/Tabuleiro.tsx
@@ -6,7 +6,7 @@ export default function Tabuleiro(props: propsTabuleiro){
     console.log(props.inverso)
     return (
         <div className={`
-        flex flex-row-${props.inverso} justify-center items-center
+        flex ${props.inverso} justify-center items-center
         `}>
             <div className="h-24 w-24 bg-white"></div>
             <div className="h-24 w-24 bg-black"></div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div className="h-screen w-full flex justify-center items-center bg-slate-500">
       <div className="flex flex-col">
-        <Tabuleiro />
-        <Tabuleiro inverso="reverse" />
-        <Tabuleiro />
-        <Tabuleiro inverso="reverse" />
-        <Tabuleiro />
-        <Tabuleiro inverso="reverse" />
-        <Tabuleiro />
-        <Tabuleiro inverso="reverse" />
+        <Tabuleiro inverso="flex-row" />
+        <Tabuleiro inverso="flex-row-reverse" />
+        <Tabuleiro inverso="flex-row" />
+        <Tabuleiro inverso="flex-row-reverse" />
+        <Tabuleiro inverso="flex-row" />
+        <Tabuleiro inverso="flex-row-reverse" />
+        <Tabuleiro inverso="flex-row" />
+        <Tabuleiro inverso="flex-row-reverse" />
       </div>
     </div>
   );
